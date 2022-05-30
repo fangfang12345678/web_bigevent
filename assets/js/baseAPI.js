2d2aea6e59e7f8d9d396583d07368694079e1d55
fangfang12345678 <2748556369@qq.com>
2022-05-30 16:04:56 +0800

    完成了登录和注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,19 +6,5 @@ $.ajaxPrefilter((option) => {
   option.url = `http://www.liulongbin.top:3007` + option.url;
 
   // 统一为有权限的接口，设置 headers 请求头
-  if (option.url.indexOf("/my/")) {
-    option.headers = {
-      Authorization: localStorage.getItem("token"),
-    };
-  }
 
-  option.complete = (res) => {
-    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
-     //  强制清空 token
-     localStorage.removeItem("token");
-     // 强制跳转到登录页面
-     location.href = "/login.html"
-    }
- }
 });
